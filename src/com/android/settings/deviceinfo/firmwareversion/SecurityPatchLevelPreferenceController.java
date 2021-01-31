ccf95628c7bf3dc34b998f19dea7409f66a76acd
CannedShroud <hypersync112@gmail.com>
2021-01-31 10:03:03 +0000

    Settings: Allow devices to force custom sec patch
    * CTS is tied to fp. To pass with stock FP the same sec patch date is necessary. Here we allow devices to overlay witch patch to display.
      This is not a full proof solution and devices maintainers can use it for the worse by faking patch dates but those guys do not have any
      ethics anyway.

--- src/com/android/settings/deviceinfo/firmwareversion/SecurityPatchLevelPreferenceController.java
+++ src/com/android/settings/deviceinfo/firmwareversion/SecurityPatchLevelPreferenceController.java
@@ -24,12 +24,14 @@ import android.text.TextUtils;
 import android.util.Log;
 
 import androidx.preference.Preference;
+import com.android.settings.R;
 
 import com.android.settings.core.BasePreferenceController;
 import com.android.settingslib.DeviceInfoUtils;
 
 public class SecurityPatchLevelPreferenceController extends BasePreferenceController {
 
+    private static String mCustomPatch;
     private static final String TAG = "SecurityPatchCtrl";
     private static final Uri INTENT_URI_DATA = Uri.parse(
             "https://source.android.com/security/bulletin/");
@@ -51,7 +53,13 @@ public class SecurityPatchLevelPreferenceController extends BasePreferenceContro
 
     @Override
     public CharSequence getSummary() {
-        return mCurrentPatch;
+        if (mContext.getResources().getBoolean(R.bool.config_needsCustomPatch)) {
+                mCustomPatch = mContext.getResources().getString(R.string.custom_sec_patch);
+        }
+        else {
+            mCustomPatch = mCurrentPatch;
+        }
+        return mCustomPatch;
     }
 
     @Override
