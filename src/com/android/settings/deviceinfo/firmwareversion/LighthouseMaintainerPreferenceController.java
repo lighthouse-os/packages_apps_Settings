49ca3066fa4ad3757125452375cf43262c5b66e2
Jackeagle <jackeagle102@gmail.com>
2021-04-08 13:28:58 +0900

    Settings: Switch to string overlay for maintainer string
    - Define following overlay in your device tree
        <string name="build_maintainer_summary"></string>
    
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- src/com/android/settings/deviceinfo/firmwareversion/LighthouseMaintainerPreferenceController.java
+++ src/com/android/settings/deviceinfo/firmwareversion/LighthouseMaintainerPreferenceController.java
@@ -2,9 +2,8 @@ package com.android.settings.deviceinfo.firmwareversion;
 
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
+import android.content.res.Resources;
 import android.net.Uri;
-import android.os.SystemProperties;
 import android.text.TextUtils;
 
 import androidx.preference.Preference;
@@ -14,16 +13,11 @@ import com.android.settings.core.BasePreferenceController;
 
 public class LighthouseMaintainerPreferenceController extends BasePreferenceController {
 
-    private static final String KEY_BUILD_MAINTAINER_URL = "ro.lighthouse.build.donate_url";
-
-    private final PackageManager mPackageManager;
-
     private String mDeviceMaintainer;
 
     public LighthouseMaintainerPreferenceController(Context context, String key) {
         super(context, key);
-        mDeviceMaintainer = SystemProperties.get("ro.lighthouse.build.maintainer");
-        mPackageManager = mContext.getPackageManager();
+        mDeviceMaintainer = mContext.getResources().getString(R.string.build_maintainer_summary);
     }
 
     @Override
@@ -35,20 +29,5 @@ public class LighthouseMaintainerPreferenceController extends BasePreferenceCont
     @Override
     public CharSequence getSummary() {
         return mDeviceMaintainer;
-    }
-
-    @Override
-    public boolean handlePreferenceTreeClick(Preference preference) {
-        String maintainerUrl = SystemProperties.get(KEY_BUILD_MAINTAINER_URL,
-                mContext.getString(R.string.unknown));
-        if (!TextUtils.equals(preference.getKey(), getPreferenceKey())) {
-            return false;
-        }
-
-        final Intent intent = new Intent();
-        intent.setAction(Intent.ACTION_VIEW);
-        intent.setData(Uri.parse(maintainerUrl));
-        mContext.startActivity(intent);
-        return true;
     }
 }
